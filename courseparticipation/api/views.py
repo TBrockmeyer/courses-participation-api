b5f088a26d6689610174844e0d2e9ef650c6559f
t.brockmeyer <t.brockmeyer@criteo.com>
2020-10-17 18:17:54 +0200

    Add first basic API test (request to /courses/), and code that passes it

--- courseparticipation/api/views.py
+++ courseparticipation/api/views.py
@@ -1,4 +1,12 @@
-#from django.shortcuts import render
+from django.core.wsgi import get_wsgi_application
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'courseparticipation.settings')
+
+application = get_wsgi_application()
+
+from django.conf import settings
+if not settings.configured:
+    settings.configure()
 
 from api.models import Course
 from rest_framework import generics
---
+++ courseparticipation/tests.py
@@ -0,0 +1,28 @@
+import unittest
+
+from api.views import CourseList
+
+from django.core.wsgi import get_wsgi_application
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'courseparticipation.settings')
+
+application = get_wsgi_application()
+
+from django.conf import settings
+if not settings.configured:
+    settings.configure()
+
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, APITestCase
+
+class TestApi(APITestCase):
+
+    def test_course_list(self):
+        view = CourseList.as_view()
+        factory = APIRequestFactory()
+        request = factory.get('/courses/')
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+if __name__ == '__main__':
+    unittest.main()
