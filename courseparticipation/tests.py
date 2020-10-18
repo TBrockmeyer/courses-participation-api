e4d83ff0708e88a6e25aed35fcc4625e9048887a
t.brockmeyer <t.brockmeyer@criteo.com>
2020-10-18 17:26:26 +0200

    Prepare tests.py for the User object related tests

--- courseparticipation/tests.py
+++ courseparticipation/tests.py
@@ -3,6 +3,8 @@ import unittest
 from api.views import CourseList
 from api.generate_db_entries import DbEntriesCreation
 
+from django.contrib.auth.models import User
+
 from django.core.wsgi import get_wsgi_application
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'courseparticipation.settings')
@@ -20,7 +22,10 @@ class TestApi(APITestCase):
 
     def test_course_list(self):
         view = CourseList.as_view()
-        user_creation = DbEntriesCreation.create_user_admin()
+        #DbEntriesCreation().create_user_admin()
+        #DbEntriesCreation().create_user_examples(10)
+        #user_entries = User.objects.all()
+        #print(user_entries.order_by("-date_joined").values())
         factory = APIRequestFactory()
         request = factory.get('/courses/')
         response = view(request)
