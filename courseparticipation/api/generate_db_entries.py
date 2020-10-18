2a6bba672604cb2aaa8e59f8ac5b522f38f371cc
t.brockmeyer <t.brockmeyer@criteo.com>
2020-10-18 17:25:15 +0200

    Remove not needed __init__ method from DbEntriesCreation Class

--- courseparticipation/api/generate_db_entries.py
+++ courseparticipation/api/generate_db_entries.py
@@ -10,25 +10,21 @@ if not settings.configured:
 
 from django.contrib.auth.models import User
 
-class DbEntriesCreation():
-    def __init__(self):
-        self.tmp = 0
-    # self only needed if self.objects passed:
-    # https://stackoverflow.com/questions/12179271/meaning-of-classmethod-and-staticmethod-for-beginner
-    def create_user(username, password, is_superuser, is_staff):
-        user=User.objects.create_user(username, password=password)
-        user.is_superuser=True
-        user.is_staff=True
-        user.save()
+class DbEntriesCreation:
+    def create_user(self, username, password, is_superuser, is_staff):
+        self.user=User.objects.create_user(username, password=password)
+        self.user.is_superuser=is_superuser
+        self.user.is_staff=is_staff
+        self.user.save()
 
-    def create_user_admin():
-        user_creation = self.create_user('admin', 'admin', True, True)
+    def create_user_admin(self):
+        self.user_creation = self.create_user('admin', 'admin', True, True)
 
-    def create_user_examples(number_examples):
-        user_examples_usernames = ["user_"+str(i) for i in range (0,number_examples)]
-        user_examples_passwords = ["user"+str(i)+"_pw" for i in range (0,number_examples)]
+    def create_user_examples(self, number_examples):
+        self.user_examples_usernames = ["user_"+str(i) for i in range (0,number_examples)]
+        self.user_examples_passwords = ["user"+str(i)+"_pw" for i in range (0,number_examples)]
         for j in range (0, number_examples):
             try:
-                self.create_user(user_examples_usernames[j], user_examples_passwords[j], False, False)
+                self.create_user(self.user_examples_usernames[j], self.user_examples_passwords[j], False, False)
             except:
                 pass
