4b7c833555159b31065ca761f45eae6d023567c5
t.brockmeyer <t.brockmeyer@criteo.com>
2020-10-19 09:32:01 +0200

    Improved user creation tests in tests.py to check filtered user objects (to allow for other users existing in the database)

--- courseparticipation/tests.py
+++ courseparticipation/tests.py
@@ -32,22 +32,33 @@ class TestApi(APITestCase):
     def test_user_creation_admin(self):
         view = UserList.as_view()
         DbEntriesCreation().create_user_admin()
+
+        user_objects_list = list(User.objects.filter(username__startswith="test_admin").all().values())
+
         factory = APIRequestFactory()
         request = factory.get('/users/')
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data[0]['username'], 'admin')
+        self.assertEqual(user_objects_list[0]["username"], 'test_admin')
 
     def test_user_creation_10_users_list(self):
         view = UserList.as_view()
         number_users = 10
         DbEntriesCreation().create_user_examples(number_users)
+
+        user_objects_list = list(User.objects.filter(username__startswith="user_").all().values())
+
         factory = APIRequestFactory()
         request = factory.get('/users/')
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         for i in range (0, number_users):
-            self.assertEqual(response.data[i]['username'], 'user_'+str(i))
+            self.assertEqual(user_objects_list[i]['username'], 'user_'+str(i))
+
+    # Test scope: user permissions
+    ## Only allow admins to create courses
+    #def test_permission_course_creation(self):
+
 
 if __name__ == '__main__':
     unittest.main()
