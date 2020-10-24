6f6c6bc1e2e5992d9a14a34657dbfcf1d7d92052
t.brockmeyer <t.brockmeyer@criteo.com>
2020-10-24 16:59:04 +0200

    Revert "Added Participation model, view and url configuration to handle course participation objects"
    This reverts commit 7b8f22dde509812764f1ec932c644ddebefa82bb.

--- courseparticipation/api/models.py
+++ courseparticipation/api/models.py
@@ -3,16 +3,6 @@ from django.db import models
 class Course(models.Model):
     course_id = models.AutoField(unique=True, primary_key=True)
     course_title = models.CharField(max_length=100, blank=True, default='')
-    participations = models.CharField(max_length=100, blank=True, default='')
     
     def save(self, *args, **kwargs):
         super(Course, self).save(*args, **kwargs)
-
-
-class Participation(models.Model):
-    participation_id = models.AutoField(unique=True, primary_key=True)
-    participation_course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-    user = models.ForeignKey('auth.User', unique=True, on_delete=models.CASCADE)
-
-    def save(self, *args, **kwargs):
-        super(Participation, self).save(*args, **kwargs)
