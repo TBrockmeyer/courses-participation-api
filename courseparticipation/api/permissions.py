010f9717469b3a526d5cc336b100bfa4352dd983
t.brockmeyer <t.brockmeyer@criteo.com>
2020-10-27 21:51:02 +0100

    Cleaned permissions.py of irrelevant comments and obsolete has_permission method

--- courseparticipation/api/permissions.py
+++ courseparticipation/api/permissions.py
@@ -21,19 +21,10 @@ class IsOwnerOrAdmin(permissions.BasePermission):
     Custom permission to only allow owners of an object or admins to edit it.
     """
 
-    """
-    # Read permissions are allowed to any authenticated request,
-    # so we'll always allow GET, HEAD or OPTIONS requests.
-    if request.method in permissions.SAFE_METHODS and request.user.is_authenticated:
-        return True
-    """
-
-    def has_permission(self, request, view):
-        return True
-
     def has_object_permission(self, request, view, obj):
-        # (Read and write) permissions are only allowed to admins and the owner of the participation.
-        if (obj.participation_user_id == request.user or request.user.is_staff):
+        # TODO: ensure that permissions allow only the user that created this participation (and the admin),
+        # but not other users to delete a participation
+        if (obj.user_id == request.user.id or request.user.is_staff):
             return True
         else:
             return False
