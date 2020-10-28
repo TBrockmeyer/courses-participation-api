7751717b4074174ec7387a5d28e1ecb5213878f1
t.brockmeyer <t.brockmeyer@criteo.com>
2020-10-28 08:32:08 +0100

    Modified IsOwnerOrAdmin in permissions.py to handle user_id argument correctly in has_permission, i.e. also for view classes that don't call has_object_permission

--- courseparticipation/api/permissions.py
+++ courseparticipation/api/permissions.py
@@ -18,9 +18,26 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     """
+    Custom permission to only allow authenticated users or admins to create an object for the given user.
+    """
+    def has_permission(self, request, view):
+        # If requesting user is an admin, view and creation permissions are conceded
+        if(request.user.is_staff):
+            return True
+        # If a user_id is given by a non-admin user as an argument in the request,
+        # it needs to be the user's own ID
+        if ('user_id' in request.data):
+            if(int(request.data['user_id']) == request.user.id):
+                return True
+            else:
+                return False
+        # If no user_id given in request, the retrieval of the user will be handled in views.py
+        return True
+
+        
+    """
     Custom permission to only allow owners of an object or admins to edit it.
     """
-
     def has_object_permission(self, request, view, obj):
         # TODO: ensure that permissions allow only the user that created this participation (and the admin),
         # but not other users to delete a participation
