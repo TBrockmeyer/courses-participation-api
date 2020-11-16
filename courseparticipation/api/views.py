38ee3acd1532ca3529dd5e53e1957a305fe670d4
t.brockmeyer <t.brockmeyer@criteo.com>
2020-11-16 08:38:08 +0100

    Ensure that only one Participation can be created per user, i.e. a request to create a second participation for a user is denied.

--- courseparticipation/api/views.py
+++ courseparticipation/api/views.py
@@ -9,7 +9,7 @@ if not settings.configured:
     settings.configure()
 
 from api.models import Course, Participation
-from rest_framework import generics, status
+from rest_framework import generics, status, exceptions
 
 from api.serializers import CourseSerializer, UserSerializer, ParticipationSerializer
 from api.generate_db_entries import DbEntriesCreation
@@ -54,21 +54,31 @@ class ParticipationUpdate(generics.CreateAPIView):
     serializer_class = ParticipationSerializer
     permission_classes = [IsOwnerOrAdmin]
 
+    # Users call this endpoint indicating a participation_course_id and a Participation_course_phase.
+    # TODO: ensure that a participation_course_phase is within the available range of phases
+    # TODO: ensure that a Participation cannot be created if a user has any Participation
+    # (needs to unsubscribe there / or indicate new phase if already in Course)
+    """
+    Check given user_id: does have existing participation?
+    ├─ No ► Create new participation with requested user_id, course_id and course_phase
+    └─ Yes ► Is requested course_phase same as that of existing participation?
+        ├─ No ► Refuse request with error  and inform in response that old participation needs to be deleted first
+        └─ Yes ► Is requested course_phase adjoining step to that of existing participation?
+            ├─ No ► Refuse request with error  and inform in response that only jumps to adjoining phases allowed
+            └─ Yes ► update info in database by saving data from request through serializer
+    """
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        existing_participation = Participation.objects.filter(user_id=int(self.request.data['user_id']))
+        if (existing_participation.count() > 0):
+            message = "A Participation for this user_id already exists. Delete unwanted participation first through /delete/<int:pk> endpoint."
+            raise exceptions.ValidationError(detail=message)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     def perform_create(self, serializer):
-        # Users call this endpoint indicating a participation_course_id and a Participation_course_phase.
-        # TODO: ensure that a participation_course_phase is within the available range of phases
-        # TODO: ensure that a participation_course_phase cannot be given when the user isn't yet in the Course
-        # TODO: ensure that a Participation cannot be created if a user has any Participation
-        # (needs to unsubscribe there / or indicate new phase if already in Course)
-        """
-        Check given user_id: does have existing participation?
-        ├─ No ► Create new participation with requested user_id, course_id and course_phase
-        └─ Yes ► Is requested course_phase same as that of existing participation?
-            ├─ No ► Refuse request with error  and inform in response that old participation needs to be deleted first
-            └─ Yes ► Is requested course_phase adjoining step to that of existing participation?
-                ├─ No ► Refuse request with error  and inform in response that only jumps to adjoining phases allowed
-                └─ Yes ► update info in database by saving data from request through serializer
-        """
         relevant_user_id = self.request.user.id
         if ('user_id' in self.request.data):
             relevant_user_id=int(self.request.data['user_id'])
