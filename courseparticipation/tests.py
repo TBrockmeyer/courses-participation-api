9c715cfc63c04111e9b8f3be7930768f44fc7b2f
t.brockmeyer <t.brockmeyer@criteo.com>
2020-12-20 14:05:41 +0100

    Added new test to test.py: test time updates to Course objects upon users entering course

--- courseparticipation/tests.py
+++ courseparticipation/tests.py
@@ -1,7 +1,7 @@
 import unittest
 
-from api.views import CourseList, UserList, CourseList, ParticipationCreation, ParticipationList, ParticipationUpdate
-from api.models import Participation
+from api.views import UserList, CourseList, ParticipationCreation, ParticipationList, ParticipationUpdate, CourseUpdateRuntime
+from api.models import Course, Participation
 
 from api.generate_db_entries import DbEntriesCreation
 
@@ -18,7 +18,9 @@ if not settings.configured:
     settings.configure()
 
 from rest_framework import status
-from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate
+from rest_framework.test import APIRequestFactory, APITestCase, APISimpleTestCase, force_authenticate
+
+import time
 
 class TestApi(APITestCase):
     # Helper methods:
@@ -150,6 +152,106 @@ class TestApi(APITestCase):
         # Participation update by test user should fail if updating course id and course phase
         response_c = self.create_test_participation_update_response(test_user, response_participation_creation.data['participation_id'], response_course_2_creation.data['course_id'], 1)
         self.assertEqual(response_c.status_code, status.HTTP_400_BAD_REQUEST)
+
+class TestApiParticipationUpdate(APITestCase):
+
+    # Helper methods:
+    def auth_test_admin(self):
+        auth_username_admin = "test_admin"
+        auth_password_admin = "test_admin"
+        DbEntriesCreation().create_user_admin(auth_username_admin, auth_password_admin)
+        return User.objects.get(username=auth_username_admin)
+
+    def auth_test_user(self):
+        auth_user_unique_string = "test_user_" + str(int(round(time.time() * 1000)))
+        auth_username_user = auth_user_unique_string
+        auth_password_user = auth_user_unique_string
+        DbEntriesCreation().create_user(auth_username_user, auth_password_user, False, False)
+        return User.objects.get(username=auth_username_user)
+
+    def create_test_course_response(self, user, course_title, course_phases):
+        view = CourseList.as_view()
+        factory = APIRequestFactory()
+        request = factory.post('', {'course_title': course_title, 'course_phases': course_phases})
+        force_authenticate(request, user=user)
+        return view(request)
+
+    def create_test_participation_response(self, user, participation_course_id, participation_course_phase):
+        view = ParticipationCreation.as_view()
+        factory = APIRequestFactory()
+        request = factory.post('', {'participation_course_id': participation_course_id, 'participation_course_phase': participation_course_phase})
+        force_authenticate(request, user=user)
+        return view(request)
+
+    def create_test_participation_update_response(self, user, participation_id, participation_course_id, participation_course_phase):
+        view = ParticipationUpdate.as_view()
+        factory = APIRequestFactory()
+        request = factory.put('', {'participation_course_id': participation_course_id, 'participation_course_phase': participation_course_phase})
+        force_authenticate(request, user=user)
+        return view(request, pk=str(participation_id))
+
+    def create_test_course_runtime_update_response(self, user, course_id, course_runtime=-1):
+        view = CourseUpdateRuntime.as_view()
+        factory = APIRequestFactory()
+        request = factory.put('', {'course_id': course_id, 'course_runtime': course_runtime})
+        force_authenticate(request, user=user)
+        return view(request, pk=str(course_id))
+
+    def get_test_course_response(self, user):
+        view = ParticipationList.as_view()
+        factory = APIRequestFactory()
+        request = factory.get('/courses/')
+        force_authenticate(request, user=user)
+        return view(request)
+
+    def get_test_phases(self):
+        return "['Lobby Start', 'Welcome', 'Warmup', 'Push', 'Cooldown', 'Goodbye', 'Lobby End']"
+
+    # Test fixtures
+    def setUp(self):
+        self.test_admin = self.auth_test_admin()
+        self.test_user = self.auth_test_user()
+        self.test_user_2 = self.auth_test_user()
+        self.test_course = self.create_test_course_response(self.test_admin, 'Test Course Participation', self.get_test_phases())
+        self.test_participation_course_phase_initial = 0
+        self.test_participation = self.create_test_participation_response(
+            self.test_user,
+            self.test_course.data['course_id'],
+            self.test_participation_course_phase_initial
+        )
+        self.test_participation_2 = self.create_test_participation_response(
+            self.test_user_2,
+            self.test_course.data['course_id'],
+            self.test_participation_course_phase_initial
+        )
+
+    def test_participation_update_first_users(self):
+        participation_id = self.test_participation.data['participation_id']
+        participation_2_id = self.test_participation_2.data['participation_id']
+        participation_course_id = self.test_participation.data['participation_course_id']
+        participation_course_phase = 1
+
+        # Participation update with entering a nonlobby phase by first test user (if updating course phase only) should
+        # ├─ update course_starttime
+        # └─ not update course_runtime
+        course_object_values_pre = Course.objects.filter(course_id=participation_course_id).values()[0]
+        delay_user = 1.0
+        time.sleep(delay_user)
+        response = self.create_test_participation_update_response(self.test_user, participation_id, participation_course_id, participation_course_phase)
+        course_object_values_post = Course.objects.filter(course_id=participation_course_id).values()[0]
+
+        self.assertNotEqual(course_object_values_post['course_starttime'], course_object_values_pre['course_starttime'])
+        self.assertEqual(course_object_values_post['course_runtime'], 0)
+
+        # Participation update with entering a nonlobby phase by second test user (if updating course phase only) should
+        # ├─ not update course_starttime
+        # └─ update course_runtime to specified delay_user_2
+        delay_user_2 = 1.0
+        time.sleep(delay_user_2)
+        response = self.create_test_participation_update_response(self.test_user_2, participation_2_id, participation_course_id, participation_course_phase)
+        course_object_values_post_2 = Course.objects.filter(course_id=participation_course_id).values()[0]
+        self.assertEqual(course_object_values_post_2['course_starttime'], course_object_values_post['course_starttime'])
+        self.assertEqual(course_object_values_post_2['course_runtime'], delay_user_2)
 
 
 if __name__ == '__main__':
