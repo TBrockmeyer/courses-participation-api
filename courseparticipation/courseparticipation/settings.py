fbed7fec273cd7fa853e07d586d567665f026053
t.brockmeyer <t.brockmeyer@criteo.com>
2020-12-27 12:26:15 +0100

    Corrected STATIC_URL in settings.py

--- courseparticipation/courseparticipation/settings.py
+++ courseparticipation/courseparticipation/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'courseparticipation/staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'courseparticipation/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
