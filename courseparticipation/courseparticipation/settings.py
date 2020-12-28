ad873f5a2af633b5a929a356f326ce9f731639b3
t.brockmeyer <t.brockmeyer@criteo.com>
2020-12-28 22:14:47 +0100

    Adjusted settings.py: set Debug to False and provided allowed hosts

--- courseparticipation/courseparticipation/settings.py
+++ courseparticipation/courseparticipation/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+q55f0p-qm61@0!w)v01js+awlgtg+#w3v6ob=4u9hxyz=()5i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
